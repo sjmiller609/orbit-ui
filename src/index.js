1fff0616072d0c6dbd75040720722d833aca807d
pcmaffey <peter@astronomer.io>
2018-05-15 13:29:16 -0600

    Configured apollo

--- src/index.js
+++ src/index.js
@@ -4,35 +4,52 @@ import Root from './modules/App/Root';
 //import storage from './helpers/storage';
 //import { handleErrors } from './helpers/handleErrors';
 
-// ==== Who knows if I'm doing this right
-import { ApolloClient, createNetworkInterface, ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
+import { ApolloLink } from 'apollo-link';
 
-
+import { ApolloProvider } from 'react-apollo';
 import { render } from 'react-dom';
 
-const networkInterface = createNetworkInterface({
-  uri: window.API_HTTP
+const cache = new InMemoryCache();
+
+const authMiddleware = new ApolloLink((operation, forward) => {
+  const token = localStorage.getItem('token') || "";
+  const organization = localStorage.getItem('organization') || "";
+  operation.setContext({
+    headers: {
+      authorization: token,
+      organization,
+    }
+  });
+
+  return forward(operation);
 });
 
-networkInterface.use([{
-  applyMiddleware(req, next) {
-    if (!req.options.headers) {
-      req.options.headers = {};  // Create the header object if needed.
-    }
-    // get the authentication token from local storage if it exists
-    req.options.headers.authorization = localStorage.getItem('token') || "";
-    req.options.headers.organization = localStorage.getItem('organization') || "";
-    next();
-  }
-}]);
+const httpLink = new HttpLink({
+  uri: window.API_HTTP,
+  credentials: 'include'
+});
 
-networkInterface.useAfter([{
-//  applyAfterware: handleErrors,
-}]);
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.map(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+      ),
+    );
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
 
 const client = new ApolloClient({
-  networkInterface,
-  dataIdFromObject: r => r.id,
+  link: ApolloLink.from([
+    errorLink,
+    authMiddleware,
+    httpLink
+  ]),
+  cache
 });
 
 render(
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -36,7 +36,7 @@ export default {
       inject: true,
       api_http: 'http://houston.peter-dev.astronomer.io/v1',
       stripe_pk: 'pk_test_9nakxMiaKT3egTTtCmdfLdet',
-      tracking_snippet: '!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Astronomer snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t){var e=document.createElement("script");e.type="text/javascript";e.async=!0;e.src=("https:"===document.location.protocol?"https://":"http://")+"cdn.astronomer.io/analytics.js/v1/"+t+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(e,n)};analytics.SNIPPET_VERSION="3.1.0";analytics.load("sourceId");}}();'
+      tracking_snippet: ''
     })
   ],
   module: {
