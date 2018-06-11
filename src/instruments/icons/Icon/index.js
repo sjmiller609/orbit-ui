942723521ebd8b32ff6c9d87708597d0629c142b
pcmaffey <peter@astronomer.io>
2018-06-11 15:30:25 -0600

    Add dynamic import pattern to icon

--- src/instruments/icons/Icon/index.js
+++ src/instruments/icons/Icon/index.js
@@ -5,9 +5,33 @@ import classnames from 'classnames'
 
 import s from './styles.scss'
 
-const Icon = ({ icon, className }) => {
-  const src = require(`./${icon}.svg`)
-  return <img src={src} className={classnames(s.icon, className)} />
+// const Icon = ({ icon, className }) => {
+//   // const src = require(`./${icon}.svg`)
+//   // return <img src={src} className={classnames(s.icon, className)} />
+//   import(`./${icon}.svg`).then(src => (
+//     <img src={src} className={classnames(s.icon, className)} />
+//   ))
+//   return null
+// }
+
+class Icon extends React.Component {
+  state = {
+    src: null,
+  }
+
+  componentWillMount() {
+    import(`./${this.props.icon}.svg`).then(src =>
+      this.setState({ src: src.default })
+    )
+  }
+
+  render() {
+    const { src } = this.state
+    if (!src) return null
+    return (
+      <img src={src} className={classnames(s.icon, this.props.className)} />
+    )
+  }
 }
 
 Icon.propTypes = {
