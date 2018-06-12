7b51fe59dd2b814b765a4c90d1b315353d28e963
pcmaffey <peter@astronomer.io>
2018-06-12 12:49:43 -0600

    Add basic error boundary
    https://github.com/astronomerio/orbit/issues/15

--- src/modules/App/Root/index.js
+++ src/modules/App/Root/index.js
@@ -5,16 +5,19 @@ import { hot } from 'react-hot-loader'
 import Routes from '../Routes'
 import UI from '../UI'
 import { ProviderUI } from '../../../instruments'
+import ErrorBoundary from '../ErrorBoundary'
 
 const Root = () => {
   return (
     <ProviderUI>
-      <BrowserRouter>
-        <React.Fragment>
-          <Routes />
-          <UI />
-        </React.Fragment>
-      </BrowserRouter>
+      <ErrorBoundary>
+        <BrowserRouter>
+          <React.Fragment>
+            <Routes />
+            <UI />
+          </React.Fragment>
+        </BrowserRouter>
+      </ErrorBoundary>
     </ProviderUI>
   )
 }
---
+++ src/modules/app/ErrorBoundary/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Snackbar } from '../../../instruments'
+import PropTypes from 'prop-types'
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { hasError: false }
+  }
+
+  componentDidCatch(error, info) {
+    // Display fallback UI
+    this.setState({ hasError: true })
+
+    console.log(error)
+    console.log(info)
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return <h1>Something went wrong.</h1>
+    }
+    return this.props.children
+  }
+}
+
+ErrorBoundary.propTypes = {
+  children: PropTypes.element,
+}
+
+export default ErrorBoundary
