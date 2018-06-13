ff6000cd68c6fc9021a497ce5144b87576c890ca
pcmaffey <peter@astronomer.io>
2018-06-13 13:42:31 -0600

    Optimize Link component

--- src/instruments/links/Link/index.js
+++ src/instruments/links/Link/index.js
@@ -1,7 +1,7 @@
 'use strict'
 import PropTypes from 'prop-types'
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink as NavLink1, Link as NavLink2 } from 'react-router-dom'
 import s from './styles.scss'
 import classnames from 'classnames'
 import { Icon } from '../../../instruments'
@@ -12,6 +12,7 @@ const Link = ({
   style,
   to,
   className,
+  activeClassName,
   newTab,
   arrow,
   backArrow,
@@ -20,12 +21,15 @@ const Link = ({
   const arr = arrow && <Icon className={s.arrow} icon="arrow" />
   const backArr = backArrow && <Icon className={s.backArrow} icon="arrow" />
   const A = props => <a {...props} />
+  // only use ReactRouter NavLink if activeClassName (for performance)
+  const NavLink = activeClassName ? NavLink1 : NavLink2
   const Component = to ? NavLink : A
   return (
     <Component
       {...props}
       to={to}
       className={classnames(s.link, s[style], className)}
+      activeClassName={activeClassName}
       onClick={onClick}
       target={newTab ? '_blank' : null}>
       <React.Fragment>
@@ -44,6 +48,7 @@ Link.propTypes = {
   newTab: PropTypes.bool,
   to: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
   className: PropTypes.string,
+  activeClassName: PropTypes.string,
   arrow: PropTypes.bool,
   backArrow: PropTypes.bool,
 }
