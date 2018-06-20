9c5c7782e65b89f479d9abe64918aaf9c1baf051
pcmaffey <peter@astronomer.io>
2018-06-20 13:12:09 -0600

    Teams api

---
+++ src/modules/teams/Data/api.js
@@ -0,0 +1,52 @@
+import gql from 'graphql-tag'
+
+const team = gql`
+  fragment team on Team {
+    id: uuid
+    label
+    description
+    active
+    created_at
+    updated_at
+  }
+`
+
+export default {
+  Teams: gql`
+    query teams($teamId: Uuid, $userId: Uuid) {
+      teams(teamUuid: $teamId, userUuid: $userId) {
+        ...team
+      }
+    }
+    ${team}
+  `,
+  CreateTeam: gql`
+    mutation createTeam(
+      $label: String!
+      description: String
+    ) {
+      createTeam(
+        label: $label
+        description: $description
+      ) {
+        ...team
+      }
+    }
+    ${team}
+  `,
+  UpdateTeam: gql`
+    mutation updateTeam($id: Uuid!, $payload: JSON!) {
+      updateTeam(teamUuid: $id, payload: $payload) {
+        ...team
+      }
+    }
+    ${team}
+  `,
+  DeleteTeam: gql`
+    mutation deleteTeam($id: Uuid!) {
+      deleteTeam(teamUuid: $id) {
+        id
+      }
+    }
+  `,
+}
