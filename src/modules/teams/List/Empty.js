7e5d23a87c5bce73d63fc3fc78d5812ad6f1c656
pcmaffey <peter@astronomer.io>
2018-06-28 09:38:58 -0600

    When no teams, show instructions

--- src/modules/teams/List/Empty.js
+++ src/modules/teams/List/Empty.js
@@ -3,14 +3,14 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import s from './styles.scss'
-import { Block1, Airflow } from 'instruments'
+import { Block1, Icon } from 'instruments'
 
 const Empty = ({ button }) => {
   return (
     <Block1
-      left={<Airflow className={s.airflowImg} dagSize={s.dag} />}
-      title="Ready for takeoff?"
-      text="Deploy a private instance of Apache Airflow in minutes, and get straight to writing custom workflows.">
+      left={<Icon icon="stars" className={s.largeStars} />}
+      title="Create your Workspace"
+      text="Invite your team, launch deployments, and manage all your Airflow instances in one place.">
       {button}
     </Block1>
   )
