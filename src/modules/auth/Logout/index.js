97a7088211048d582e2fb65d618311fe582301ef
pcmaffey <peter@astronomer.io>
2018-07-03 13:19:29 -0600

    Reset Apollo cache on logout

--- src/modules/auth/Logout/index.js
+++ src/modules/auth/Logout/index.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
+import { withApollo } from 'react-apollo'
 import { Redirect, SetData, SetUI } from 'instruments'
 
-const Logout = ({ setData, setUI }) => {
+const Logout = ({ client, setData, setUI }) => {
   setData.auth(null)
-  setData.teamId(null)
   setUI.snackbar("You've successfully logged out.")
+  // remove Apollo store cache
+  client.resetStore()
+
   return <Redirect to="/login" />
 }
 
 Logout.propTypes = {
   setData: PropTypes.object,
   setUI: PropTypes.object,
+  client: PropTypes.object,
 }
 
-export default SetUI(SetData(Logout), { snackbar: true })
+export default withApollo(
+  SetUI(SetData(Logout, { auth: true }), { snackbar: true })
+)
