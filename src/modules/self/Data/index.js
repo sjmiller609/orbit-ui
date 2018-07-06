43cec11c5cd9b4303887ccf53802b8f6f737b63f
pcmaffey <peter@astronomer.io>
2018-07-06 12:50:08 -0600

    Add basic protection for invlaid token

--- src/modules/self/Data/index.js
+++ src/modules/self/Data/index.js
@@ -3,13 +3,16 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import api from './api'
 
-import { Query } from 'instruments'
+import { Query, Redirect } from 'instruments'
+
+const onError = <Redirect to="/logout/silent" />
 
 const Data = Component => {
   const Data = ({ skip, ...props }) => {
     return (
-      <Query gql={api.Self} skip={skip}>
+      <Query gql={api.Self} skip={skip} onError={onError}>
         {({ data: { self } }) => {
+          if (!self) return onError // invalid token protection
           const newProps = {
             ...props,
             self,
