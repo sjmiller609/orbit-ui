eeaec1e0d7a92350300acb4f3d670d2c0212b26c
pcmaffey <peter@astronomer.io>
2018-07-09 09:31:32 -0600

    Better error handling for when no connection

--- src/modules/auth/Data/index.js
+++ src/modules/auth/Data/index.js
@@ -3,12 +3,14 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import api from './api'
 
-import { Query } from 'instruments'
+import { Query, P } from 'instruments'
+
+const onError = <P>Service not available. Please try again later.</P>
 
 const Data = Component => {
   const Data = ({ vars, ...otherProps }) => {
     return (
-      <Query gql={api.AuthConfig} vars={vars}>
+      <Query gql={api.AuthConfig} vars={vars} onError={onError}>
         {({ data: { authConfig } }) => {
           const newProps = {
             ...otherProps,
