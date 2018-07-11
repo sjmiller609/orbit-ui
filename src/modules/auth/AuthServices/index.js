a0add77ad8104d3d5d69178246b3f3cf2e685ef1
pcmaffey <peter@astronomer.io>
2018-07-11 16:11:06 -0600

    Add explicit redirect from oauth based on query param "rd"

--- src/modules/auth/AuthServices/index.js
+++ src/modules/auth/AuthServices/index.js
@@ -2,6 +2,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { withRouter } from 'react-router-dom'
+import { getParams } from 'helpers/url'
 
 import Buttons from './Buttons'
 
@@ -10,19 +11,22 @@ class AuthServices extends React.Component {
   componentWillMount() {
     let to = '/'
 
-    const ref = document.referrer
-    const host = window.location.host
-      .split('.')
-      .splice(1)
-      .join('.')
-
-    // if same root domain
-    if (ref && host && ~ref.indexOf(host)) to = ref
-
     const { location } = this.props
     const from = location.state && location.state.from
+    const params = getParams(location.search)
     if (from) to = from
-    else to = location.pathname
+    else if (params.rd) to = params.rd
+    else {
+      const ref = document.referrer
+      const host = window.location.host
+        .split('.')
+        .splice(1)
+        .join('.')
+
+      // if same root domain
+      if (ref && host && ~ref.indexOf(host)) to = ref
+      else to = location.pathname
+    }
 
     // gets encoded on server
     this.onSuccess += to
