04189d33d65c6060097acedbb4943aa8b3781874
pcmaffey <peter@astronomer.io>
2018-07-26 13:41:21 -0600

    Add 'waiting for logs'

--- src/modules/logs/List/index.js
+++ src/modules/logs/List/index.js
@@ -2,15 +2,19 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import s from './styles.scss'
-import { Table, Console } from 'instruments'
+import { Table, Console, LoadingDots, Mini } from 'instruments'
 
 import Item from './Item'
-import Empty from './Empty'
 
 const List = ({ deployment, logs, search }) => {
   return (
     <Table className={s.list} search={search} Container={Console}>
       {logs && logs.map((l, i) => <Item key={l.id || i} log={l} />)}
+      {(!logs || !logs.length) && (
+        <Mini className={s.waiting}>
+          Waiting for logs<LoadingDots />
+        </Mini>
+      )}
     </Table>
   )
 }
