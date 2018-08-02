c2030d84eed388a462fadb392fd7d5471f084082
pcmaffey <peter@astronomer.io>
2018-08-02 15:10:42 -0600

    Update link for onclick

--- src/instruments/links/Link/index.js
+++ src/instruments/links/Link/index.js
@@ -31,11 +31,19 @@ const Link = ({
   // only use ReactRouter NavLink if activeClassName (for performance)
   const NavLink = props.activeClassName ? NavLink1 : NavLink2
   /* eslint-disable react/display-name */
+  /* eslint-disable no-unused-vars */
+  /* eslint-disable react/prop-types */
   const Component =
     to && !externalUrl(to)
       ? NavLink
-      : props => (
-          <a {...props} href={to} target={newTab === false ? null : '_blank'} />
+      : ({ to, newTab, className, onClick, children }) => (
+          <a
+            onClick={onClick}
+            className={className}
+            href={to}
+            target={newTab === false ? null : '_blank'}>
+            {children}
+          </a>
         )
   return (
     <Component {...newProps}>
