5dd22d82df8fcc9810b0fc053234a633746ebdad
pcmaffey <peter@astronomer.io>
2018-08-15 09:41:00 -0600

    Login user on verify

--- src/modules/auth/Data/Verify.js
+++ src/modules/auth/Data/Verify.js
@@ -1,17 +1,31 @@
 'use strict'
 import React from 'react'
+import PropTypes from 'prop-types'
+
 import api from './api'
 
-import { Mutation, Redirect } from 'instruments'
+import { Mutation, Redirect, SetData } from 'instruments'
 
 const Verify = Component => {
-  const Verify = props => {
+  const Verify = ({ setData, ...props }) => {
     return (
       <Mutation
         gql={api.VerifyEmail}
         success="Email successfuly verified"
         errorMsg="Your confirmation token is not valid"
         OnError={<Redirect to="/" />}
+        onSuccess={data => {
+          console.log(data)
+          if (!data) return
+          if (data.token) {
+            const { value, payload } = data.token
+            // pass token to context
+            setData.auth({
+              token: value,
+              exp: payload.exp,
+            })
+          }
+        }}
         track="User Verfies Email">
         {({ mutate }) => {
           const newProps = {
@@ -30,8 +44,11 @@ const Verify = Component => {
       </Mutation>
     )
   }
+  Verify.propTypes = {
+    setData: PropTypes.object,
+  }
 
-  return Verify
+  return SetData(Verify, { auth: true })
 }
 
 export default Verify
