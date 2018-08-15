d5dcabab4e5957f9a016fd21366a7e1faf70c186
pcmaffey <peter@astronomer.io>
2018-08-15 15:35:14 -0600

    Forgot to commit these routes

--- src/modules/workspaces/Routes/index.js
+++ src/modules/workspaces/Routes/index.js
@@ -3,24 +3,24 @@ import { Load } from 'instruments'
 export default [
   {
     path: '/workspaces',
-    component: Load(() => import('../Workspaces')),
+    component: Load(() => import('../Workspaces').then(c => c.default)),
     exact: true,
   },
   {
     path: '/workspaces/new',
-    component: Load(() => import('../New')),
+    component: Load(() => import('../New').then(c => c.default)),
     exact: true,
   },
   {
     path: '/workspaces/:id',
-    component: Load(() => import('../SetWorkspace')),
+    component: Load(() => import('../SetWorkspace').then(c => c.default)),
   },
 ]
 
 export const workspace = [
   {
     path: '/settings',
-    component: Load(() => import('../Settings')),
+    component: Load(() => import('../Settings').then(c => c.default)),
     exact: true,
   },
 ]
