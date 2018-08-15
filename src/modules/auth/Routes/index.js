855fb6137c35f1f20cafe1ef89f574eb32e5f6fd
pcmaffey <peter@astronomer.io>
2018-08-15 15:35:24 -0600

    Revert "Update routes as possible fix for https://github.com/astronomerio/orbit-ui/issues/44"
    This reverts commit 498d26305361aa373699c2f3e8403b9cc64f3332.

--- src/modules/auth/Routes/index.js
+++ src/modules/auth/Routes/index.js
@@ -4,27 +4,27 @@ import { Load } from 'instruments'
 export default [
   {
     path: '/(|signup)',
-    component: Load(() => import('../Signup').then(c => c.default)),
+    component: Load(() => import('../Signup')),
     exact: true,
   },
   {
     path: '/login',
-    component: Load(() => import('../Login').then(c => c.default)),
+    component: Load(() => import('../Login')),
     exact: true,
   },
   {
     path: '/confirm',
-    component: Load(() => import('../Confirm').then(c => c.default)),
+    component: Load(() => import('../Confirm')),
     exact: true,
   },
   {
     path: '/reset-password',
-    component: Load(() => import('../ResetPw').then(c => c.default)),
+    component: Load(() => import('../ResetPw')),
     exact: true,
   },
   {
     path: '/verify',
-    component: Load(() => import('../Verify').then(c => c.default)),
+    component: Load(() => import('../Verify')),
     exact: true,
   },
 ]
@@ -32,32 +32,32 @@ export default [
 export const otherAuthRoutes = [
   {
     path: '/oauth',
-    component: Load(() => import('../OauthSuccess').then(c => c.default)),
+    component: Load(() => import('../OauthSuccess')),
     exact: true,
   },
   {
     path: '/token',
-    component: Load(() => import('../OauthSuccess/Token').then(c => c.default)),
+    component: Load(() => import('../OauthSuccess/Token')),
     exact: true,
   },
   {
     path: '/forgot-password',
-    component: Load(() => import('../ForgotPw').then(c => c.default)),
+    component: Load(() => import('../ForgotPw')),
     exact: true,
   },
   {
     path: '/forgot-password/sent',
-    component: Load(() => import('../ForgotPw/Sent').then(c => c.default)),
+    component: Load(() => import('../ForgotPw/Sent')),
     exact: true,
   },
   {
     path: '/resend',
-    component: Load(() => import('../Resend').then(c => c.default)),
+    component: Load(() => import('../Resend')),
     exact: true,
   },
 
   {
     path: '/logout',
-    component: Load(() => import('../Logout').then(c => c.default)),
+    component: Load(() => import('../Logout')),
   },
 ]
--- src/modules/deployments/Routes/index.js
+++ src/modules/deployments/Routes/index.js
@@ -3,17 +3,17 @@ import { Load } from 'instruments'
 export default [
   {
     path: '/deployments',
-    component: Load(() => import('../Deployments').then(c => c.default)),
+    component: Load(() => import('../Deployments')),
     exact: true,
   },
   {
     path: '/deployments/new',
-    component: Load(() => import('../New').then(c => c.default)),
+    component: Load(() => import('../New')),
     exact: true,
   },
   {
     path: '/deployments/:id',
-    component: Load(() => import('../Deployment').then(c => c.default)),
+    component: Load(() => import('../Deployment')),
     // matches sub routes in Deployment
   },
 ]
