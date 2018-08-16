43eeb1acc93e8d712252245b5bf326e96d09907e
pcmaffey <peter@astronomer.io>
2018-08-16 16:08:22 -0600

    Update prod webpack

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,14 +1,18 @@
 // For info about this file refer to webpack and webpack-hot-middleware documentation
 // For info on how we're generating bundles with hashed filenames for cache busting: https://medium.com/@okonetchnikov/long-term-caching-of-static-assets-with-webpack-1ecb139adb95#.w99i89nsz
 import webpack from 'webpack'
-import ExtractTextPlugin from 'extract-text-webpack-plugin'
 import WebpackMd5Hash from 'webpack-md5-hash'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
+import CleanWebpackPlugin from 'clean-webpack-plugin'
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+
 import { default as common, output } from './webpack.common'
 
 export default {
   ...common,
-  devtool: 'eval',
+  // devtool: 'cheap-module-eval-source-map', // NOTE: This dramatically increases bundle size and is the biggest difference with build-prod. Useful for debugging
+  devtool: 'eval', // switch to this for accurate bundle sizes
+
   mode: 'production',
   output: {
     ...output,
@@ -16,12 +20,15 @@ export default {
     chunkFilename: '[name].[chunkhash].js',
   },
   plugins: [
+    new CleanWebpackPlugin(['dist']),
     // Hash the files using MD5 so that their names change when the content changes.
     new WebpackMd5Hash(),
-
-    // Generate an external css file with a hash in the filename
-    new ExtractTextPlugin('[name].[md5:contenthash:hex:20].css'),
-
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: '[name].[chunkhash].css',
+      chunkFilename: '[id].[chunkhash].css',
+    }),
     // Generate HTML file that contains references to generated bundles. See here for how this works: https://github.com/ampedandwired/html-webpack-plugin#basic-usage
     new HtmlWebpackPlugin({
       template: 'src/index.ejs',
