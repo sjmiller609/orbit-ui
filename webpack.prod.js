7f0d47875e1ad3545824986eb74b8180c4838fd7
pcmaffey <peter@astronomer.io>
2018-08-17 09:10:08 -0600

    Revert source map for smaller bundle size

--- webpack.prod.js
+++ webpack.prod.js
@@ -10,8 +10,7 @@ import { default as common, output } from './webpack.common'
 
 export default {
   ...common,
-  // devtool: 'cheap-module-eval-source-map', // NOTE: This dramatically increases bundle size and is the biggest difference with build-prod. Useful for debugging
-  devtool: 'eval', // switch to this for accurate bundle sizes
+  devtool: 'nosources-source-map',
 
   mode: 'production',
   output: {
