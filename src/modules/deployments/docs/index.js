5fcc1f578c658a2a36b8e75db5d936d4fa63da9a
pcmaffey <peter@astronomer.io>
2018-08-27 12:06:40 -0600

    Parse list of env vars from airflow.cfg

--- src/modules/deployments/docs/index.js
+++ src/modules/deployments/docs/index.js
@@ -1,5 +1,21 @@
 import workers from './workers'
+import { blob } from './env'
 
 export default {
   ...workers,
 }
+
+const m1 = blob.match(/\n(.*?) =/g)
+const m2 = m1.map(m =>
+  m
+    .slice(1)
+    .replace(' =', '')
+    .replace('#', '')
+    .trim()
+)
+
+const m3 = {}
+m2.forEach(m => {
+  if (!~m.indexOf('Ex:')) m3[m] = ''
+})
+console.log(JSON.stringify(m3))
