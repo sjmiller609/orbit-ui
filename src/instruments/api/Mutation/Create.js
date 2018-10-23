c1fd3ecb45623245a925a18f23020fe926086673
pcmaffey <peter@astronomer.io>
2018-10-23 11:01:09 -0600

    Catch cache read error on create

--- src/instruments/api/Mutation/Create.js
+++ src/instruments/api/Mutation/Create.js
@@ -12,17 +12,23 @@ const Create = ({ query, ...props }) => {
         // use first key to auto grab Document name
         const result = data.data[Object.keys(data.data)[0]]
         // NOTE: this will error if not found, but apollo already handles it
-        const results = cache.readQuery({
-          query: query.name,
-          variables: query.vars,
-        })
-        console.log(data)
-        if (!results) return
-        cache.writeQuery({
-          query: query.name,
-          variables: query.vars,
-          data: { [query.type]: results[query.type].concat([result]) },
-        })
+        try {
+          const results = cache.readQuery({
+            query: query.name,
+            variables: query.vars,
+          })
+          console.log(data)
+          if (!results) return
+          cache.writeQuery({
+            query: query.name,
+            variables: query.vars,
+            data: { [query.type]: results[query.type].concat([result]) },
+          })
+        } catch (error) {
+          // don't report error when apollo tries to update cache query that hasn't been run yet (usually happens when someone navs directly to create page or refreshes and kills cached queries)
+          if (~error.toString().indexOf("Can't find field")) return
+          console.log(error)
+        }
       }}
       {...props}
     />
