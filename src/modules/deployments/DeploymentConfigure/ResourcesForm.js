e23d1d9cfd008aac984a1857c2bf5540b35c9c93
pcmaffey <peter@astronomer.io>
2018-10-31 13:07:37 -0600

    Fix lazy loading of config form

--- src/modules/deployments/DeploymentConfigure/ResourcesForm.js
+++ src/modules/deployments/DeploymentConfigure/ResourcesForm.js
@@ -10,62 +10,71 @@ import Executor from './Executor'
 import Resource from './Resource'
 import Usage from './Usage'
 
-const ResourcesForm = ({ form, deploymentConfig, loaded }) => {
-  return (
-    <CardForm
-      ref={() => loaded('resources')}
-      title="Configure Components"
-      button={{
-        save: form.save,
-        text: 'Update',
-      }}
-      className={s.card}>
-      <FormSection id="exeuctor">
-        <Executor form={form} deploymentConfig={deploymentConfig} />
-      </FormSection>
-      <FormSection id="components">
-        <Resource
-          label="Webserver"
-          field={form.field('config.webserver.resources.limits')}
-          defaultValue={deploymentConfig.defaults.webserver.resources.requests}
-          max={deploymentConfig.limits.webserver.resources.limits}
-          info={info.webserver}
-          required
-          astroUnit={deploymentConfig.astroUnit}
-        />
-        <Resource
-          label="Scheduler"
-          field={form.field('config.scheduler.resources.limits')}
-          defaultValue={deploymentConfig.defaults.scheduler.resources.requests}
-          max={deploymentConfig.limits.scheduler.resources.limits}
-          info={info.scheduler}
-          required
-          astroUnit={deploymentConfig.astroUnit}
-        />
-      </FormSection>
+class ResourcesForm extends React.Component {
+  componentDidMount() {
+    this.props.loaded('resources')
+  }
+  render() {
+    const { form, deploymentConfig, loaded } = this.props
+    return (
+      <CardForm
+        title="Configure Components"
+        button={{
+          save: form.save,
+          text: 'Update',
+        }}
+        className={s.card}>
+        <FormSection id="executor">
+          <Executor form={form} deploymentConfig={deploymentConfig} />
+        </FormSection>
+        <FormSection id="components">
+          <Resource
+            label="Webserver"
+            field={form.field('config.webserver.resources.limits')}
+            defaultValue={
+              deploymentConfig.defaults.webserver.resources.requests
+            }
+            max={deploymentConfig.limits.webserver.resources.limits}
+            info={info.webserver}
+            required
+            astroUnit={deploymentConfig.astroUnit}
+          />
+          <Resource
+            label="Scheduler"
+            field={form.field('config.scheduler.resources.limits')}
+            defaultValue={
+              deploymentConfig.defaults.scheduler.resources.requests
+            }
+            max={deploymentConfig.limits.scheduler.resources.limits}
+            info={info.scheduler}
+            required
+            astroUnit={deploymentConfig.astroUnit}
+          />
+        </FormSection>
 
-      <FormSection id="resources" title="Resources">
-        <Resource
-          label="Extra Capacity"
-          field={form.field('properties.astro_units')}
-          defaultValue={0}
-          min={0}
-          max={100}
-          step={1}
-          info={info.astroUnit}
-          convertValue={null}
-          astroUnit={deploymentConfig.astroUnit}
-        />
-        <Usage
-          extra={form.field('properties.astro_units').value}
-          config={form.field('config').value}
-          deploymentConfig={deploymentConfig}
-          executor={form.field('config.executor').value}
-          info={info.resourcesUpdate}
-        />
-      </FormSection>
-    </CardForm>
-  )
+        <FormSection id="resources" title="Resources">
+          <Resource
+            label="Extra Capacity"
+            field={form.field('properties.astro_units')}
+            defaultValue={0}
+            min={0}
+            max={100}
+            step={1}
+            info={info.astroUnit}
+            convertValue={null}
+            astroUnit={deploymentConfig.astroUnit}
+          />
+          <Usage
+            extra={form.field('properties.astro_units').value}
+            config={form.field('config').value}
+            deploymentConfig={deploymentConfig}
+            executor={form.field('config.executor').value}
+            info={info.resourcesUpdate}
+          />
+        </FormSection>
+      </CardForm>
+    )
+  }
 }
 
 ResourcesForm.propTypes = {
