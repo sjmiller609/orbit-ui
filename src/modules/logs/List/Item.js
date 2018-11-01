67f4ab3b7f45681bca456aa22d281da8a1b93ef8
pcmaffey <peter@astronomer.io>
2018-11-01 15:22:47 -0600

    Fix log stylings

--- src/modules/logs/List/Item.js
+++ src/modules/logs/List/Item.js
@@ -3,19 +3,16 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'classnames'
 import s from './styles.scss'
-import { TableRow, Box, Mini } from 'instruments'
+import { Mini } from 'instruments'
 import { format } from './helpers'
 
 const Item = ({ log, className }) => {
   const log2 = format(log.log, s)
-  const columns = [
-    <Box key="0" align="flex-start" className={s.log}>
-      <Mini>{Array.isArray(log2) ? log2.map(el => el) : log2}</Mini>
-    </Box>,
-  ]
 
   return (
-    <TableRow className={classnames(s.item, className)} columns={columns} />
+    <Mini className={classnames(s.log, className)}>
+      {Array.isArray(log2) ? log2.map(el => el) : log2}
+    </Mini>
   )
 }
 
