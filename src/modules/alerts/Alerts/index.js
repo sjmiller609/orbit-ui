2e0a8c2e5b1d21ccbd4532f8486379c85bb4f83f
pcmaffey <peter@astronomer.io>
2018-11-01 10:28:44 -0600

    Convert emails to array

--- src/modules/alerts/Alerts/index.js
+++ src/modules/alerts/Alerts/index.js
@@ -2,10 +2,19 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Emails from '../Emails'
 
+const convertEmails = emails => {
+  if (!emails) return []
+  if (typeof emails === 'string') {
+    return emails.split(',')
+  }
+  return emails
+}
+
 class Alerts extends React.Component {
   render() {
     const { deployment } = this.props
-    const emails = deployment.properties.alert_emails || []
+    const emails = convertEmails(deployment.properties.alert_emails)
+    console.log(emails)
     return (
       <Emails
         data={{
