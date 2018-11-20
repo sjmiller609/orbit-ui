c85d742fc232b8d5733140ed2a8a6b7382bf4c67
Andy Cooper <andycooper.s@gmail.com>
2018-11-20 16:19:27 -0500

    Swap tshirt size in favor of slider

--- src/modules/deployments/DeploymentConfigure/CeleryConfig.js
+++ src/modules/deployments/DeploymentConfigure/CeleryConfig.js
@@ -1,56 +1,53 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import s from './styles.scss'
-import { NumberField, Select, FormSubSection } from 'instruments'
+// import s from './styles.scss'
+import { NumberField, FormSubSection } from 'instruments'
 import info from '../info'
 
+import Resource from './Resource'
+
 import {
-  workerSizeConvert,
-  workerSizeInfo,
+  // workerSizeConvert,
+  // workerSizeInfo,
   workerTerminationUnits,
   workerTerminationConvert,
 } from './helpers'
 
-import Selector from './Selector'
-
-const workerSizes = [
-  {
-    icon: 'alien_ship',
-    value: 'small',
-    text: 'Small',
-    className: s.sizeS,
-  },
-  {
-    icon: 'alien_ship',
-    value: 'medium',
-    text: 'Medium',
-    className: s.sizeM,
-  },
-  {
-    icon: 'alien_ship',
-    value: 'large',
-    text: 'Large',
-    className: s.sizeL,
-  },
-]
+// const workerSizes = [
+//   {
+//     icon: 'alien_ship',
+//     value: 'small',
+//     text: 'Small',
+//     className: s.sizeS,
+//   },
+//   {
+//     icon: 'alien_ship',
+//     value: 'medium',
+//     text: 'Medium',
+//     className: s.sizeM,
+//   },
+//   {
+//     icon: 'alien_ship',
+//     value: 'large',
+//     text: 'Large',
+//     className: s.sizeL,
+//   },
+// ]
 
 const CeleryConfig = ({
   form,
-  deploymentConfig: { defaults, limits, presets },
+  deploymentConfig: { defaults, limits, presets, astroUnit },
 }) => {
   return (
     <FormSubSection title="Celery Executor Config">
-      <Select
-        {...form.field('config.workers.resources')}
-        label="Worker Size"
-        className={s.selectors}
-        defaultValue={presets.workerSizes.small}
-        Component={Selector}
-        options={workerSizes}
-        info={workerSizeInfo(presets.workerSizes)}
-        convert={(size, out) =>
-          workerSizeConvert(size, out, presets.workerSizes)
-        }
+      <Resource
+        label="Workers"
+        field={form.field('config.workers.resources.limits')}
+        defaultValue={defaults.workers.resources.requests}
+        max={limits.workers.resources.limits}
+        info={presets.workerSizes}
+        required
+        astroUnit={astroUnit}
       />
       <NumberField
         label="Worker Count"
