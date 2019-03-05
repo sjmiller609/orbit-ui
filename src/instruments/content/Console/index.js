0f8505f89d3b72d3d25bc546ae53ece383375a63
schnie <greg@astronomer.io>
2019-03-05 12:59:20 -0500

    Initial commit

--- src/instruments/content/Console/index.js
+++ src/instruments/content/Console/index.js
@@ -6,23 +6,23 @@ import s from './styles.scss'
 
 class Console extends React.Component {
   componentDidMount() {
-    this.elem.addEventListener('wheel', this.scrollHandler)
+    this.elem.addEventListener('wheel', this.scrollHandler.bind(this))
   }
 
   componentWillUnmount() {
-    this.elem.removeEventListener('wheel', this.scrollHandler)
+    this.elem.removeEventListener('wheel', this.scrollHandler.bind(this))
   }
 
   scrollHandler(e) {
-    if (e.deltaY) {
-      e.preventDefault()
-      e.currentTarget.scrollTop -=
-        parseFloat(
-          getComputedStyle(e.currentTarget).getPropertyValue('font-size')
-        ) *
-        (e.deltaY < 0 ? -1 : 1) *
-        2
-    }
+    if (!e.deltaY) return
+    e.preventDefault()
+    e.currentTarget.scrollTop -=
+      parseFloat(
+        getComputedStyle(e.currentTarget).getPropertyValue('font-size')
+      ) *
+      (e.deltaY < 0 ? -1 : 1) *
+      2
+    this.props.onWheel && this.props.onWheel(e)
   }
 
   render() {
@@ -40,6 +40,7 @@ class Console extends React.Component {
 Console.propTypes = {
   children: PropTypes.oneOfType([PropTypes.array, PropTypes.element]),
   className: PropTypes.string,
+  onWheel: PropTypes.func,
 }
 
 export default Console
