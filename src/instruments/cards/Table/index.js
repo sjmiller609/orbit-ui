4092bf58bb0baf8995f22d37a286ad3c37877bf9
Samantha Black <supersamblack@gmail.com>
2019-05-17 18:31:59 -0400

    Backtrack `Table` updates

--- src/instruments/cards/Table/index.js
+++ src/instruments/cards/Table/index.js
@@ -2,25 +2,75 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'classnames'
+
+import { Card, Search, Button, Row } from 'instruments'
 import s from './styles.scss'
-import { Row } from 'instruments'
+import NoResults from './NoResults'
 
-const CardRow = ({ children, className }) => {
-  const blocks = Array.isArray(children) ? children : [children]
+const Table = ({
+  children,
+  search,
+  className,
+  button,
+  headerOptions,
+  nav,
+  Empty,
+  Container = React.Fragment,
+}) => {
+  const button2 = button && <Button to={button.to}>{button.text}</Button>
+  const search2 = search && (
+    <Search
+      search={search.call}
+      text={search.text}
+      placeholder={search.placeholder}
+      className={s.search}
+      noDelay={!search.delay}
+    />
+  )
+  const header = (
+    <React.Fragment>
+      {nav}
+      <Row justify="space-between" className={s.header}>
+        {search2}
+        {headerOptions}
+        {button2}
+      </Row>
+    </React.Fragment>
+  )
 
+  // If empty
+  if (Empty && (!search || !search.text) && (!children || !children.length)) {
+    return (
+      <Card className={classnames(s.table, s.empty, className)}>
+        {Empty({ button: button2 })}
+      </Card>
+    )
+  }
+
+  // If not empty
+  let count = Array.isArray(children) ? children.length : 1
+  if (!children) count = 0
   return (
-    <Row
-      className={classnames(s.row, s[blocks.length], className)}
-      align="flex-start"
-      wrap>
-      {blocks.map(el => el)}
-    </Row>
+    <Card
+      className={classnames(s.table, count === 1 && s.one, className)}
+      header={header}>
+      <Container>
+        {children}
+        {count === 0 && search.text && <NoResults />}
+      </Container>
+    </Card>
   )
 }
 
-CardRow.propTypes = {
+Table.propTypes = {
   children: PropTypes.oneOfType([PropTypes.array, PropTypes.element]),
   className: PropTypes.string,
+  search: PropTypes.object,
+  button: PropTypes.object,
+  Empty: PropTypes.func,
+  Container: PropTypes.func,
+  headerOptions: PropTypes.element,
+  nav: PropTypes.element,
 }
 
-export default CardRow
+export default Table
