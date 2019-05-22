0128789d7234d6447ce946c0588468380ba86099
Samantha Black <supersamblack@gmail.com>
2019-05-22 12:38:57 -0400

    Add accessiblity and info addons

--- src/instruments/ui/Snackbar/_story.js
+++ src/instruments/ui/Snackbar/_story.js
@@ -3,5 +3,5 @@ import { storiesOf } from '@storybook/react'
 import { Snackbar } from 'instruments'
 
 storiesOf('Instruments|UI', module).add('Snackbar', () => (
-  <Snackbar msg="Lorem ipsum dolor sit amet, consectetur adipiscing elit. " />
+  <Snackbar msg="Lorem ipsum dolor sit amet, consectetur adipiscing elit." />
 ))
