499222300704037f1d3ca985a16772043a14351d
schnie <greg@astronomer.io>
2019-06-04 15:00:11 -0400

    Remove unused import

--- src/modules/workspaces/ConfigureBilling/UpdateBilling.js
+++ src/modules/workspaces/ConfigureBilling/UpdateBilling.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import { CardForm, Form, TextField, Box, H3, Field } from 'instruments'
+import { CardForm, Form, TextField, Box, H3 } from 'instruments'
 import CardSection from './CardElement'
 
 import UpdateCard from '../Data/UpdateCard'
