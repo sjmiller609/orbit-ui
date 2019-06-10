dbaaf435f8e30f3e32d63bfe2fcd3524446700fb
Pete DeJoy <32076749+petedejoy@users.noreply.github.com>
2019-06-10 12:28:14 -0400

    Hotfix/css and errors (#84)
    * Fix card css and add error handling for Stripe test mode errors
    
    * Remove log

--- src/modules/workspaces/Data/helpers.js
+++ src/modules/workspaces/Data/helpers.js
@@ -6,6 +6,12 @@ const errors = [
     name: 'permissions',
     error: 'Contact your Workspace Admin to upgrade your permissions.',
   },
+  {
+    key: 'test mode',
+    name: 'test mode',
+    error:
+      'Your card was declined. Your request was in test mode, but used a non test (live) card.',
+  },
 ]
 
 export const handleError = error => formErrors({ error, errors })
