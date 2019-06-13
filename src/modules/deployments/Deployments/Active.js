eb5deb2ca6fe5bf0fa2397c42851b8ac343cb4c7
Pete DeJoy <pete@astronomer.io>
2019-06-13 15:25:52 -0400

    Beghin to rework paywall to read off of isSuspended rather than stripeCustomerId

--- src/modules/deployments/Deployments/Active.js
+++ src/modules/deployments/Deployments/Active.js
@@ -5,7 +5,11 @@ import PropTypes from 'prop-types'
 import s from './styles.scss'
 import { Card, Block1, Airflow, Button } from 'instruments'
 
+<<<<<<< HEAD
 const Activation = ({ title, text, canUpdateBilling }) => {
+=======
+const Activation = ({ title, text, canUpdateIAM }) => {
+>>>>>>> Beghin to rework paywall to read off of isSuspended rather than stripeCustomerId
   const button = {
     text: 'Add a Payment Method',
     to: '/billing',
@@ -17,7 +21,11 @@ const Activation = ({ title, text, canUpdateBilling }) => {
         title={title}
         text={text}
         className={s.list}>
+<<<<<<< HEAD
         <Button disabled={!canUpdateBilling} to={button.to}>
+=======
+        <Button to={button.to} disabled={!canUpdateIAM}>
+>>>>>>> Beghin to rework paywall to read off of isSuspended rather than stripeCustomerId
           {button.text}
         </Button>
       </Block1>
@@ -28,7 +36,11 @@ const Activation = ({ title, text, canUpdateBilling }) => {
 Activation.propTypes = {
   title: PropTypes.string,
   text: PropTypes.string,
+<<<<<<< HEAD
   canUpdateBilling: PropTypes.bool,
+=======
+  canUpdateIAM: PropTypes.bool,
+>>>>>>> Beghin to rework paywall to read off of isSuspended rather than stripeCustomerId
 }
 
 export default Activation
