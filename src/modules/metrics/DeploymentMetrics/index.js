a3ae600a300221c9413023d5936eff89022c1b1d
Ry Walker <ry@rywalker.com>
2019-06-19 02:31:30 -0400

    Better steps

--- src/modules/metrics/DeploymentMetrics/index.js
+++ src/modules/metrics/DeploymentMetrics/index.js
@@ -7,7 +7,7 @@ import { withRouter } from 'react-router-dom'
 class DeploymentMetrics extends React.Component {
   state = {
     since: 30,
-    step: 15,
+    step: 10,
   }
 
   setSince = since => {
@@ -16,37 +16,37 @@ class DeploymentMetrics extends React.Component {
     // switch by minutes
     switch (since) {
       case 15:
-        step = 15
+        step = 10
         break
       case 30:
-        step = 15
+        step = 10
         break
       case 60:
-        step = 15
-        break
-      case 180:
         step = 20
         break
-      case 360:
-        step = 30
-        break
-      case 720:
+      case 180:
         step = 60
         break
-      case 1440:
+      case 360:
         step = 120
         break
+      case 720:
+        step = 240
+        break
+      case 1440:
+        step = 480
+        break
       case 2880:
-        step = 300
+        step = 960
         break
       case 10080:
-        step = 1200
+        step = 3360
         break
       case 20160:
-        step = 1800
+        step = 6720
         break
       default:
-        step = 15
+        step = 10
         break
     }
 
