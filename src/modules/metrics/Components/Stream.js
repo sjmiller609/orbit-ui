b102f2f5ffc5f4527a920dcbdac007384d5a200a
Ry Walker <ry@rywalker.com>
2019-06-19 11:00:30 -0400

    Tweak red

--- src/modules/metrics/Components/Stream.js
+++ src/modules/metrics/Components/Stream.js
@@ -60,7 +60,7 @@ class Stream extends React.Component {
 
   render() {
     const { length, data } = this.state
-    const colors = ['#C8C3BD', '#07d65b', '#049a59', '#fb5b3c'].reverse()
+    const colors = ['#C8C3BD', '#07d65b', '#049a59', '#FF5050'].reverse()
 
     return (
       <div className={s.streamContainer}>
