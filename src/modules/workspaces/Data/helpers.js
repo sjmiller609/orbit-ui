912cc20334e0e1845674041030cad9bb9294a649
Pete DeJoy <32076749+petedejoy@users.noreply.github.com>
2019-06-21 12:05:58 -0400

    Fix lint issue (#93)

--- src/modules/workspaces/Data/helpers.js
+++ src/modules/workspaces/Data/helpers.js
@@ -12,6 +12,12 @@ const errors = [
     error:
       'Your card was declined. Your request was in test mode, but used a non test (live) card.',
   },
+  {
+    key: 'Invalid email address',
+    name: 'billingEmail',
+    error:
+      "That email doesn't look right. Please try with a valid email address.",
+  },
 ]
 
 export const handleError = error => formErrors({ error, errors })
