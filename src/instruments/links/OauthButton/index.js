39c79d4d62d768107c4b10d6dea73ec770ec19c1
Ash Berlin-Taylor <ash_github@firemirror.com>
2019-07-12 23:37:36 +0100

    Update look of Login/Sign up pages to support Generic providers (#113)
    With the new change in Orbit we now allow user-configurable auth
    providers so we need a style that will work for those. This re-vamps the
    design following Chris's advice
    
    This also moves the provider logos from <img> tags to background images.

--- src/instruments/links/OauthButton/index.js
+++ src/instruments/links/OauthButton/index.js
@@ -1,16 +1,10 @@
 'use strict'
 import PropTypes from 'prop-types'
 import React from 'react'
-import { Link, Row, Box, Icon, LoadImg } from 'instruments'
+import { Link, Row } from 'instruments'
 import s from './styles.scss'
+import flex from '../../flex/Flex/styles.scss'
 import classnames from 'classnames'
-
-const images = {
-  google: LoadImg(() => import(`./img/google.svg`)),
-  github: LoadImg(() => import(`./img/github.svg`)),
-  auth0: LoadImg(() => import(`./img/auth0.svg`)),
-  okta: LoadImg(() => import('./img/okta.png')),
-}
 
 const OauthButton = ({
   service,
@@ -20,26 +14,20 @@ const OauthButton = ({
   className,
   ...props
 }) => {
-  const Img = images[service]
+  const action = login ? 'Login' : 'Sign up'
   return (
-    <Link
-      {...props}
-      to={to}
-      title={' with ' + displayName}
-      newTab={false}
-      className={classnames(s.button, s[service], className)}>
-      <Row full justify="flex-start">
-        {Img && (
-          <Box className={s.img}>
-            <Img />
-          </Box>
-        )}
-        <span className={s.text}>
+    <Row full className={classnames(className)}>
+      <Link
+        {...props}
+        to={to}
+        title={action + ' with ' + displayName}
+        newTab={false}
+        className={classnames(s.button, s[service])}>
+        <span>
           {login ? 'Login' : 'Sign up'} with {displayName}
-          <Icon className={s.arrow} icon="arrow_darkBg" />
         </span>
-      </Row>
-    </Link>
+      </Link>
+    </Row>
   )
 }
 
