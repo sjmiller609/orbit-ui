a7b9a3d8346dd6076a15cd3868881a6573030099
Pete DeJoy <32076749+petedejoy@users.noreply.github.com>
2019-08-22 15:14:53 -0400

    Hotfix: Tool Tips (#150)
    * Fix text on tool tips and center trial bar button text
    
    * Add single quotes
    
    * Fix text
    
    * Remove new css from trialbar

--- src/modules/deployments/info/text.js
+++ src/modules/deployments/info/text.js
@@ -2,17 +2,17 @@ export default {
   workerSize:
     'Determines what resources (CPU and memory) are allocated to each celery worker.',
   workerCount:
-    'How many celery workers you can send tasks to. By default, each worker is configured to handle up to 16 tasks at once. However, this will depend on what your tasks are actually doing.',
+    'How many celery workers you can send tasks to. By default, each worker is configured to handle up to 16 tasks at once.',
   workerTermination:
-    'Workers restart after each deploy to make sure they are looking at the most up-to-date code. If a deploy is triggered while a worker is executing a task, the termination grace period determines how long the worker will keep executing the task before restarting.',
+    'Workers restart after each deploy to make sure they are looking at updated code. If a deploy is triggered while a worker is executing a task, the termination grace period determines how long the worker will keep executing the task before restarting.',
   astroUnit:
-    'Astro Units define the additional resource quotas (capacity) for each deployment.',
+    'Astronomer Units define the additional resource quotas (capacity) for each deployment.',
   executor:
-    'Airflow supports multiple executor plugins. These plugins determine how and where tasks are executed. We support the Local Executor for light or test workloads, and the Celery Executor for larger, production workloads. The Celery Executor uses a distributed task queue and a scalable worker pool. Support for the Kubernetes Executor is coming soon.',
+    'Airflow supports multiple executor plugins. These plugins determine how and where tasks are executed. We support the Local Executor for light or test workloads, and the Celery and Kubernetes Executors for larger, production workloads. The Celery Executor uses a distributed task queue and a scalable worker pool, whereas the Kubernetes Executor launches every task in a separate Kubernetes pod.',
   resourcesNew:
-    'Your current configuration requires the following resources. You can manage capacities after deployment (eg. to use Pod Operator, increase memory, etc.).',
+    'Your current configuration requires the following resources. You can manage capacities after deployment if you would like to to use the Kubernetes Pod Operator, increase memory, etc.',
   webserver:
-    'Manage the resource allocations for the webserver in Astro Units (AU).',
+    'Manage the resource allocations for the webserver in Astronomer Units (AU).',
   scheduler:
-    'Manage the resource allocations for the scheduler in Astro Units (AU).',
+    'Manage the resource allocations for the scheduler in Astronomer Units (AU).',
 }
