43b40f166d1939bc1b61256051ed46b2a7229f41
Pete DeJoy <32076749+petedejoy@users.noreply.github.com>
2019-09-24 22:36:48 -0400

    Stripe customer exists without card (#165)
    * Add handling to updatebilling form for case in which customer exists in stripe without a credit card
    
    * Fix linting issue
    
    * Add schnies suggestions
    
    * Use cardNumber to format prefix
    
    * Fall back to string instead of null

--- src/modules/workspaces/ConfigureBilling/UpdateBilling.js
+++ src/modules/workspaces/ConfigureBilling/UpdateBilling.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import { CardForm, Form, TextField, Box, H3 } from 'instruments'
+import { CardForm, Form, TextField, Box, H3, Mini } from 'instruments'
 import CardSection from './CardElement'
 
 import UpdateCard from '../Data/UpdateCard'
@@ -16,9 +16,11 @@ import s from './styles.scss'
 class UpdateBilling extends React.Component {
   render() {
     const { form, card } = this.props
-    const cardNumber = '************' + card.last4
-    const expirationDate = card.expMonth + '/' + card.expYear
-    const cardBrand = card.brand.toLowerCase() //Stripe returns with a capital first letter adn the component requires a lowercase string
+    const cardNumber = card ? '************' + card.last4 : ''
+    const expirationDate = card ? card.expMonth + '/' + card.expYear : ''
+    const cardBrand = card ? card.brand.toLowerCase() : '' //Stripe returns with a capital first letter adn the component requires a lowercase string
+    const noCardMessage =
+      'We do not have a credit card on file for this workspace.'
     return (
       <CardForm
         title="Billing"
@@ -26,25 +28,28 @@ class UpdateBilling extends React.Component {
           save: form.save,
           text: 'Submit',
         }}>
-        <Box>
-          <H3>Current Card on File</H3>
-          <div className={s.cardPadding}>
-            <PaymentCard
-              bank="default"
-              brand={cardBrand}
-              number={cardNumber}
-              holderName={card.name}
-              expiration={expirationDate}
-              className={s.card}
-            />
-          </div>
-        </Box>
+        {card ? (
+          <Box>
+            <H3>Current Card on File</H3>
+            <div className={s.cardPadding}>
+              <PaymentCard
+                bank="default"
+                brand={cardBrand}
+                number={cardNumber}
+                holderName={card.name}
+                expiration={expirationDate}
+                className={s.card}
+              />
+            </div>
+          </Box>
+        ) : null}
         <Box>
           <H3>Update Your Information</H3>
+          {card ? null : <Mini>{noCardMessage}</Mini>}
           <TextField
             name="Company"
             type="text"
-            placeholder={card.company}
+            placeholder={card ? card.company : 'Company Name'}
             label="Company"
             {...form.field('company')}
             focus
@@ -52,7 +57,7 @@ class UpdateBilling extends React.Component {
           <TextField
             name="Billing Email"
             type="text"
-            placeholder={card.billingEmail}
+            placeholder={card ? card.billingEmail : 'Email'}
             label="Billing Email"
             {...form.field('billingEmail')}
             required
@@ -60,7 +65,7 @@ class UpdateBilling extends React.Component {
           <TextField
             name="Name"
             type="text"
-            placeholder={card.name}
+            placeholder={card ? card.name : 'Name'}
             label="Name on Card"
             {...form.field('name')}
             required
